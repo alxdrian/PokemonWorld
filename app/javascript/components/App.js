baef75816546bcd7c65d29e8e45d1977d2dfa683
alxdrian <alex_97piscis@outlook.com>
2022-05-04 15:27:28 -0500

    create base of mono rails react app

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloMessage from './Message';
+
+const App = () => {
+    return (
+        <HelloMessage name="user" />
+    );
+}
+
+export default App;
---
+++ app/javascript/components/Message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloMessage = ({ name }) => {
+    return (
+        <div>Welcome {name} to Pokemon World!</div>
+    );
+}
+
+export default HelloMessage;
